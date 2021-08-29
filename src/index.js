428f938185b439e0b55535d02c0a4bfcb0e30edf
Tim Schubach <tim.schubach@gmail.com>
2021-08-29 19:51:25 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth'
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81B9SEXh3cL._AC_UL200_SR200,200_.jpg",
+  title: "Billy Summers",
+  author: "Stephen-King",
+};
+
+
+// stateless functional component
+// always return JSX
+
+// JSX Rules
+// return single element
+// div / section / article or fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+function BookList() {
+  return (
+    <section className='bookList'>
+      <Book
+        image={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        image={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src={props.image} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
